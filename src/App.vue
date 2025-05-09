2c8a6b3553a269fd3befe7632c84046c72524125
rahmatalfikri <alfikrirahmat22@gmail.com>
2025-05-09 20:44:58 +0700

    display tasks

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script setup>
+import  { ref } from 'vue'
+
+const tasks = ref([
+  {
+    id: 1,
+    title: 'Task 1',
+    completed: false
+  },
+  {
+    id: 2,
+    title: 'Task 2',
+    completed: true
+  },
+  {
+    id: 3,
+    title: 'Task 3',
+    completed: false
+  }
+])
 </script>
 
 <template>
   <div>
-    
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }} - {{ task.completed ? 'Completed' : 'Not Completed' }}
+      </li>
+    </ul>
   </div>
 </template>
 
